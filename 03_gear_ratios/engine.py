c6cf9996f26c2252e3c9062b98db5675fb164b59
late <lauriseppalainen@gmail.com>
2023-12-03 21:06:26 +0200

    day 3 done

---
+++ 03_gear_ratios/engine.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+import sys
+import numpy as np
+curr_file = Path(__file__)
+sys.path.append(str(curr_file.parent.parent))
+from utils import read_lines, get_neighbours
+from collections import defaultdict
+
+
+# nice edgecase tester from
+# https://www.reddit.com/r/adventofcode/comments/189q9wv/2023_day_3_another_sample_grid_to_use/
+# input = """12.......*..
+# +.........34
+# .......-12..
+# ..78........
+# ..*....60...
+# 78.........9
+# .5.....23..$
+# 8...90*12...
+# ............
+# 2.2......12.
+# .*.........*
+# 1.1..503+.56"""
+input = curr_file.parent / 'input'
+lines = read_lines(input)
+grid = []
+for line in lines:
+    grid.append(list(line))
+grid = np.array(grid)
+
+# parse grid
+part_numbers = []
+max_i, max_j = grid.shape
+for i in range(max_i):
+    digits = []
+    coords = []
+    for j in range(max_j):
+        if grid[i, j].isdigit():
+            digits.append(grid[i, j])
+            coords.append((i, j))
+        if not grid[i, j].isdigit() or j == max_j-1:
+            if digits:
+                number = int("".join(digits))
+                part_numbers.append((number, coords))
+                digits = []
+                coords = []
+
+# part 1
+part_sum = 0
+for number, digit_coords in part_numbers:
+    found = False
+    coord_set = set()
+    for i, j in digit_coords:
+        neighbours, coords = get_neighbours(grid, i, j, diagonals=True)
+        coord_set = coord_set | set(coords)
+    coord_set = coord_set - set(digit_coords)
+    for coord in coord_set:
+        if (grid[coord] != '.' and not grid[coord].isdigit()):
+            part_sum += number
+            break
+        
+print(f'Sum of part numbers is {part_sum}')
+
+# part 2
+# get gear candidates
+gear_is, gear_js = np.where(grid == '*')
+gear_ratios = 0
+start_idx = 0
+for i, j in zip(gear_is, gear_js):
+    _, n_coords = get_neighbours(grid, i, j, diagonals=True)
+    overlapping_numbers = []
+    for number, number_coords in part_numbers[start_idx:]:
+        # if the number row is two above, we can move the start of the search
+        if number_coords[0][0] < i - 1:
+            start_idx += 1
+        # if the number row is two below, we will no longer find neigbouring digits
+        if number_coords[0][0] > i + 1:
+            break
+        intersection = set(n_coords) & set(number_coords)
+        if intersection:
+            overlapping_numbers.append(number)
+            if len(overlapping_numbers) > 2:
+                break
+    if len(overlapping_numbers) == 2:
+        gear_ratios += overlapping_numbers[0] * overlapping_numbers[1]
+print(f'Sum of gear ratios is {gear_ratios}')
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from typing import Union
 from pathlib import Path
+import numpy as np
 
 
 def read_lines(input: Union[Path, str]):
@@ -13,3 +14,42 @@ def read_lines(input: Union[Path, str]):
         if lines[-1] == '\n' or lines[-1] == '':
             lines = lines[:-1]
         return lines
+
+
+def get_neighbours(grid, i, j, diagonals=False, return_coords=True):
+    max_i, max_j = grid.shape
+    out = [None] * 8 if diagonals else [None] * 4
+    coords = [(i-1, j),
+              (i+1, j),
+              (i, j-1),
+              (i, j+1)]
+    if (i != 0):
+        out[0] = grid[i-1, j]
+    if (i < max_i-1):
+        out[1] = grid[i+1, j]
+    if (j != 0):
+        out[2] = grid[i, j-1]
+    if (j < max_j-1):
+        out[3] = grid[i, j+1]
+    if diagonals:
+        # NW
+        diag_coords = [(i-1, j-1), (i-1, j+1), (i+1, j+1), (i+1, j-1)]
+        coords += diag_coords
+        if (i != 0) and (j!= 0):
+            out[4] = grid[i-1, j-1]
+        # NE
+        if (i != 0) and (j != max_j - 1):
+            out[5] = grid[i-1, j+1]
+        # SE
+        if (i != max_i - 1) and (j != max_j - 1):
+            out[6] = grid[i+1, j+1]
+        # SW
+        if (i != max_i - 1) and (j != 0):
+            out[7] = grid[i+1, j-1]
+    if return_coords:
+        coords = [c for c, x in zip(coords, out) if x is not None]
+        out = [x for x in out if x is not None]
+        return out, coords
+    else:
+        out = [x for x in out if x is not None]
+        return out
