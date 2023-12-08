348b16bcbd4ac11456496dc6449e78755fe55fc4
Lauri Seppäläinen <lauri.seppalainen@helsinki.fi>
2023-12-08 14:46:18 +0200

    finish day 8

--- 08_haunted/ghost_maps.py
+++ 08_haunted/ghost_maps.py
@@ -1,3 +1,4 @@
+from math import lcm
 from pathlib import Path
 import sys
 import numpy as np
@@ -37,7 +38,6 @@ steps = 0
 for instruction in cycle(instructions):
     if pointer == goal:
         break
-    # print(f'At {pointer}. Stepping {instruction} to {tree[pointer][instruction]}')
     pointer = tree[pointer][instruction]
     steps += 1
 print(f'Took a total of {steps} steps.')
@@ -62,17 +62,44 @@ def find_cycle(start_node, tree, instructions):
         pointer = tree[pointer][instruction]
         steps_until_loop += 1
     idx_of_loop = traversed.index(pointer)
-    goal_indices = [i+1 for i, x in enumerate(traversed[idx_of_loop:]) if x[-1] == 'Z']
-    print(f'Ghost from {start_node}:')
-    print(f'\tHits first goal after {steps_until_first_goal} steps')
-    print(f'\tEnters a loop after {steps_until_loop} steps')
-    print(f'\tAfter that, hits a goal after {goal_indices} steps')
+    goal_indices = [i+1 for i, x in enumerate(traversed[idx_of_loop:])
+                    if x[-1] == 'Z']
     loop_length = len(traversed) - idx_of_loop
-    return steps_until_first_goal, loop_length
+    return steps_until_first_goal, loop_length, goal_indices
 
 
+def extended_gdc(a, b):
+    # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode
+    old_r, r = a, b
+    old_s, s = 1, 0
+    old_t, t = 0, 1
+    while r:
+        quotient, remainder = divmod(old_r, r)
+        old_r, r = r, remainder
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+    return old_r, old_s, old_t
 
-# tree, instructions = parse_maps(read_lines(input))
-pointers = [x for x  in tree.keys() if x[-1] == 'A']
+
+def sync_with_offset(period1, offset1, period2, offset2):
+    gdc, s, t = extended_gdc(period1, period2)
+    offset_diff = offset1 - offset2
+    od_mult, od_remainder = divmod(offset_diff, gdc)
+    if od_remainder:
+        raise ValueError('The ghosts will never sync!')
+    comb_period = period1 // gdc * period2
+    comb_offset = (offset1 - s * od_mult * period1) % comb_period
+    return comb_period, comb_offset
+
+
+# all both the methods above are useless
+# BECAUSE THERE IS A SECRET PROPERTY that the cycle period == offset
+# for all ghosts
+pointers = [x for x in tree.keys() if x[-1] == 'A']
+cycle_properties = []
 for s in pointers:
-    steps_to_goal, loop_length = find_cycle(s, tree, instructions)
+    _, loop_length, _ = find_cycle(s, tree, instructions)
+    cycle_properties.append(loop_length)
+
+# loop over list of cycle properties, combine as you goal
+print(f'All ghosts sync up after {lcm(*cycle_properties)} steps.')
